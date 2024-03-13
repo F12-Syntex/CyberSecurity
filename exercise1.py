d42a0bdacae1ba20425f2e6a484dac8e21d38fb8
Saifurrahmaan khan <sk902@kent.ac.uk>
2024-03-13 02:25:51 +0000

    fix(syntax error): replace trim() to strip() in exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,2 +1,9 @@
-input = open("inputfiles/personal_input/cexercise1.txt", "r").readlines()[0].trim()
+
+def decrypt_caesar_cipher(ciphertext):
+    ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return
+
+input = open("inputfiles/personal_input/cexercise1.txt", "r").readlines()[0].strip()
 print(input)
+
+decrypt_caesar_cipher(input)
